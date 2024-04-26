022e7184001ed8114114f9e51890d5c74368d5ff
Савченков Никита <savchienkov.nikita@gmail.com>
2024-04-26 15:59:22 +0300

    Ending write algorithm

--- oddEven/main.cpp
+++ oddEven/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 
+//Считывание числа с клавиатуры
+int readNumber() {
+  std::cout << "Enter integer number: ";
+  std::int16_t number;
+  std::cin >> number;
+
+  return number;
+}
+
+//Определение чётности или нечётности числа
+bool isEven(int number) {
+  if (number % 2 == 0) {
+    std::cout << number << " is even\n";
+
+    return true;
+  } else {
+    std::cout << number << " is odd\n";
+
+    return false;
+  }
+}
+
 int main() {
+  int number = readNumber();
+
+  isEven(number);
 
   return 0;
 }
