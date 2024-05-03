715599da12779635945ac1ffb85c821d3a213ab1
Савченков Никита <savchienkov.nikita@gmail.com>
2024-05-03 23:22:41 +0300

    write work algorithm
    Нужно было каждый раз перезаписывать значение number

--- convertFromDecToBin/main.cpp
+++ convertFromDecToBin/main.cpp
@@ -9,37 +9,27 @@ std::uint16_t readNumber() {
   return number;
 }
 
-std::uint16_t calculateNumber (std::uint16_t number, std::uint16_t degree) {
-  //std::cout << (number >= degree ? '1' : '0');
+// Перевод из десятичного числа в двоичное
+std::uint16_t decToBitNumber(std::uint16_t number, std::uint16_t degree) {
+  std::cout << (number >= degree ? '1' : '0');
 
-  if (number >= degree) {
-    std::cout << '1';
-  } else {
-    std::cout << '0';
-  }
-
-  if (number >= degree) {
-    return number - degree;
-  } else {
-    return number;
-  }
+  return number >= degree ? number - degree : number;
 }
 
 int main() {
   std::uint16_t number = readNumber();
 
-  calculateNumber(number, 128);
-  calculateNumber(number, 64);
-  calculateNumber(number, 32);
-  calculateNumber(number, 16);
+  number = decToBitNumber(number, 128);
+  number = decToBitNumber(number, 64);
+  number = decToBitNumber(number, 32);
+  number = decToBitNumber(number, 16);
 
   std::cout << ' ';
 
-  calculateNumber(number, 8);
-  calculateNumber(number, 4);
-  calculateNumber(number, 2);
-  calculateNumber(number, 1);
-
+  number = decToBitNumber(number, 8);
+  number = decToBitNumber(number, 4);
+  number = decToBitNumber(number, 2);
+  number = decToBitNumber(number, 1);
 
   return 0;
 }
