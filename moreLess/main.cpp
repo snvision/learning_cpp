3db63eb0f325e5dd1bf6889a7ff0b88082c8f85d
Савченков Никита <savchienkov.nikita@gmail.com>
2024-05-11 12:26:11 +0300

    update algorithm
    Требует доработки

--- moreLess/main.cpp
+++ moreLess/main.cpp
@@ -2,46 +2,26 @@
 #include <iostream>
 
 
-// Считывание первого числа с клавиатуры
-std::uint16_t readFirstNumber() {
+int main() {
+  std::uint16_t firstNumber;//Создаём firstNumber
   std::cout << "Введите первое число: ";
-  std::uint16_t firstNumber;
   std::cin >> firstNumber;
 
-  return firstNumber;
-}
-
-// Считывание второго числа с клавиатуры
-std::uint16_t readSecondNumber() {
-  std::cout << "Введите второе число больше первого: ";
-  std::uint16_t secondNumber;
+  std::uint16_t secondNumber;//создаём secondNumber
+  std::cout << "Введите второе число, больше чем первое: ";
   std::cin >> secondNumber;
 
-  return secondNumber;
-}
-
-// Меняем местами первое и второе число
-void calcResult(std::uint16_t firstNumber, std::uint16_t secondNumber) {
-  std::uint16_t moreNumber;
+  // Проверка условия для вывода чисел в порядке от меншьшего к большему
   if (firstNumber > secondNumber) {
-  std::cout << "Меняем местами\n";
-  moreNumber   = firstNumber;
-  firstNumber  = secondNumber;
-  secondNumber = moreNumber;
-  }
+    std::cout << "Меняем местами\n";
+    std::uint16_t moreNumber;//Создаём moreNumber
+    moreNumber   = firstNumber;
+    firstNumber  = secondNumber;
+    secondNumber = moreNumber;
+  }// Уничтожаем moreNumber
 
   std::cout << "Меньшее число: " << firstNumber << '\n';
-  std::cout << "Меньшее число: " << secondNumber << '\n';
-}
-
-
-int main() {
-  setlocale(LC_ALL, "rus");
-
-  std::uint16_t firstNumber  = readFirstNumber();
-  std::uint16_t secondNumber = readSecondNumber();
-
-  calcResult(firstNumber, secondNumber);
+  std::cout << "Большее число: " << secondNumber << '\n';
 
   return 0;
-}
+}// Уничтожаем firstNumber и secondNumber
