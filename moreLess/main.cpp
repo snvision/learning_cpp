2791947fbcb74d9f5fc16a387eeb6ba41643d22f
Савченков Никита <savchienkov.nikita@gmail.com>
2024-05-12 11:41:34 +0300

    work algorithm
    Применил std::pair и assert

--- moreLess/main.cpp
+++ moreLess/main.cpp
@@ -1,32 +1,42 @@
+#include <cassert>
 #include <cstdint>
 #include <iostream>
+#include <utility>
 
 
+// Чтение числа с клавиатуры
 std::uint16_t readNumber() {
   std::uint16_t number;
   std::cin >> number;
-
   return number;
+}
+
+// Меняем местами первое и второе число
+auto swapLess(std::uint16_t lhs, std::uint16_t rhs) {
+  if (lhs > rhs) {
+    std::cout << "Меняем местами\n";
+    std::uint16_t const tmp = lhs;
+    lhs                     = rhs;
+    rhs                     = tmp;
+  }
+  return std::pair{lhs, rhs};
+}
+
+void printSortNumbers(std::uint16_t lessNumber, std::uint16_t moreNumber) {
+  assert(lessNumber <= moreNumber);
+  std::cout << "Меньшее число: " << lessNumber << '\n'
+            << "Большее число: " << moreNumber << '\n';
 }
 
 int main() {
   std::cout << "Введите первое число: ";
-  std::uint16_t lessNum = readNumber();
+  std::uint16_t firstNumber = readNumber();
 
-  std::cout << "Введите второе число: ";
-  std::uint16_t moreNum = readNumber();
+  std::cout << "Введите второе число, больше первого: ";
+  std::uint16_t secondNumber = readNumber();
 
-  // Большего и меньшего числа для вывода в порядке возрастания
-  if (lessNum > moreNum) {
-    std::cout << "Меняем местами\n";
-    std::uint16_t tmp;//Создаём tmp
-    tmp          = lessNum;
-    lessNum  = tmp;
-    moreNum = tmp;
-  }// Уничтожаем tmp
-
-  std::cout << "Меньшее число: " << lessNum << '\n';
-  std::cout << "Большее число: " << moreNum << '\n';
+  auto const [lessNumber, moreNumber] = swapLess(firstNumber, secondNumber);
+  printSortNumbers(lessNumber, moreNumber);
 
   return 0;
-}// Уничтожаем firstNumber и secondNumber
+}
