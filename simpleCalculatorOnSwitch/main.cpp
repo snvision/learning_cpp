32840ea1823ebaa6832c030f204b2007e2d4424d
Савченков Никита <savchienkov.nikita@gmail.com>
2024-05-26 23:36:49 +0300

    write algorithm

--- simpleCalculatorOnSwitch/main.cpp
+++ simpleCalculatorOnSwitch/main.cpp
@@ -1,46 +1,62 @@
+#include <cstdint>
 #include <iostream>
 
 
 //Считывание числа
-double readNumber() {
-  std::cout << "Enter double type number: ";
-  double number;
+std::int64_t readNumber() {
+  std::int64_t number;
   std::cin >> number;
-
   return number;
 }
+
 
 //Считывание оператора
 char readOperator() {
   std::cout << "Enter math operator of the following: +, -, *, or /: ";
   char mathOperator;
   std::cin >> mathOperator;
-
   return mathOperator;
 }
 
-void printResult(double firstNumber, char mathOperator, double secondNumber) {
-  if (mathOperator == '+') {
-    std::cout << firstNumber << " + " << secondNumber << " = " << firstNumber + secondNumber << '\n';
-  }
-  if (mathOperator == '-') {
-    std::cout << firstNumber << " - " << secondNumber << " = " << firstNumber - secondNumber << '\n';
-  }
-  if (mathOperator == '*') {
-    std::cout << firstNumber << " * " << secondNumber << " = " << firstNumber * secondNumber << '\n';
-  }
-  if (mathOperator == '/') {
-    std::cout << firstNumber << " / " << secondNumber << " = " << firstNumber / secondNumber << '\n';
+
+// Подсчёт результата выражения в зависимости от выбранного математического оператора
+void calculate(std::int64_t firstNumber, std::int64_t secondNumber, char mathOperator) {
+  switch (mathOperator) {
+    case '+':
+      std::cout << firstNumber << " + " << secondNumber
+                << " = " << firstNumber + secondNumber << '\n';
+      break;
+    case '-':
+      std::cout << firstNumber << " - " << secondNumber
+                << " = " << firstNumber - secondNumber << '\n';
+      break;
+    case '*':
+      std::cout << firstNumber << " * " << secondNumber
+                << " = " << firstNumber * secondNumber << '\n';
+      break;
+    case '/':
+      std::cout << firstNumber << " / " << secondNumber
+                << " = " << firstNumber / secondNumber << '\n';
+      break;
+    case '%':
+      std::cout << firstNumber << " % " << secondNumber
+                << " = " << firstNumber % secondNumber << '\n';
+      break;
+    default:
+      std::cout << "Unknown operator\n";
+      break;
   }
 }
 
 
 int main() {
-  double firstNumber  = readNumber();
-  char   mathOperator = readOperator();
-  double secondNumber = readNumber();
+  std::cout << "Enter first int number: ";
+  const std::int64_t firstNumber = readNumber();
+  std::cout << "Enter second int number: ";
+  const std::int64_t secondNumber = readNumber();
+  const char         mathOperator = readOperator();
 
-  printResult(firstNumber, mathOperator, secondNumber);
+  calculate(firstNumber, secondNumber, mathOperator);
 
   return 0;
 }
