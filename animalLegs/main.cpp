965166d8ce6137d7a4c9470c83aabc0e441a748a
Савченков Никита <savchienkov.nikita@mail.ru>
2024-05-27 13:29:43 +0300

    part algorithm

--- animalLegs/main.cpp
+++ animalLegs/main.cpp
@@ -1,3 +1,43 @@
+#include <iostream>
+#include <string>
+
+// Виды животных
+enum class Animal {
+  pig,
+  chicken,
+  goat,
+  cat,
+  dog,
+  ostrich,
+};
+
+
+std::string getAnimalName(Animal animal) {
+  switch (animal) {
+    case Animal::pig:
+      return "pig";
+      break;
+    case Animal::chicken:
+      return "chicken";
+      break;
+    case Animal::goat:
+      return "goat";
+      break;
+    case Animal::cat:
+      return "cat";
+      break;
+    case Animal::dog:
+      return "dog";
+      break;
+    case Animal::ostrich:
+      return "ostrich";
+      break;
+    default:
+      return "Unknown animal";
+  }
+}
+
+
 int main() {
 
 
