ea013d9f13cade95a62b04aa196a4bb8af6996c7
Савченков Никита <savchienkov.nikita@gmail.com>
2024-06-03 21:46:50 +0300

    v1.0

--- piramidOfNumbers/main.cpp
+++ piramidOfNumbers/main.cpp
@@ -1,4 +1,22 @@
+#include <iostream>
+
+
+//Построение пирамиды из цифр
+void getPiramidOfNumbers () {
+  std::int16_t outer = 5;
+  while (outer >= 1) {
+    std::int16_t inner = outer;
+    while (inner >= 1) {
+      std::cout << inner-- << ' ';
+    }
+    --outer;
+    std::cout << '\n';
+  }
+}
+
+
 int main() {
+  getPiramidOfNumbers();
 
   return 0;
 }
