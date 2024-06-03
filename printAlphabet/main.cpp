fa62e018ee4436349689b0833f7211614103f363
Савченков Никита <savchienkov.nikita@mail.ru>
2024-06-03 16:52:08 +0300

    v1.0

--- printAlphabet/main.cpp
+++ printAlphabet/main.cpp
@@ -1,4 +1,17 @@
+#include <iostream>
+
+
+void printAlphabet() {
+  char count = 'a';
+  while (count <= 'z') {
+    std::cout << "Letter: " << count << ", ASCII-code: " << static_cast<std::int16_t>(count) << '\n';
+    ++count;
+  }
+}
+
+
 int main() {
+  printAlphabet();
 
   return 0;
 }
