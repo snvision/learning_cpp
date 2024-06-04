67b290fc0d899ac1779e70ab4d85b7ba01b47c08
Савченков Никита <savchienkov.nikita@gmail.com>
2024-06-04 23:17:01 +0300

    v1.0 write algorithm

--- invertedPiramidOfNumbers/main.cpp
+++ invertedPiramidOfNumbers/main.cpp
@@ -1,4 +1,26 @@
+#include <cstdint>
+#include <iostream>
+
+
+void printLadderOfNumbers() {
+  std::int16_t outer = 1;
+  while (outer <= 5) {
+    std::int16_t inner = 5;
+    while (inner >= 1 ) {
+      if (inner <= outer) {
+        std::cout << inner << ' ';
+      } else {
+        std::cout << "  ";
+      }
+      --inner;
+    }
+    std::cout << '\n';
+    ++outer;
+  }
+}
+
 int main() {
+  printLadderOfNumbers();
 
   return 0;
 }
