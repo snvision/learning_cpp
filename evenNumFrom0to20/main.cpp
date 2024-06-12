38b7d80b6346114c7b26569007e4958f11252d6f
Савченков Никита <savchienkov.nikita@mail.ru>
2024-06-12 13:03:26 +0300

    end write algorithm

--- evenNumFrom0to20/main.cpp
+++ evenNumFrom0to20/main.cpp
@@ -1,4 +1,17 @@
+#include <cstdint>
+#include <iostream>
+
+
+// Печать чётных чисел от 0 до 20
+void printNumbers() {
+  for (std::int16_t count = 0; count < 21; count += 2) {
+    std::cout << count << ' ';
+  }
+}
+
+
 int main() {
+  printNumbers();
 
   return 0;
 }
