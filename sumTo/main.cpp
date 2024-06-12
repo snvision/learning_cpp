6fcac44a2125c14e79d65425e2e2292e1a076214
Савченков Никита <savchienkov.nikita@mail.ru>
2024-06-12 14:08:10 +0300

    end write algorithm

--- sumTo/main.cpp
+++ sumTo/main.cpp
@@ -1,4 +1,36 @@
+#include <cstdint>
+#include <iostream>
+
+
+// Считывание значения
+std::int16_t readValue() {
+  std::cout << "Enter value: ";
+  std::int16_t value;
+  std::cin >> value;
+  return value;
+}
+
+
+// Подсчёт суммы
+std::int16_t sumTo(std::int16_t value) {
+  auto sumValue = 0;
+  for (auto count = 0; count <= value; ++count) {
+    sumValue += count;
+  }
+  return sumValue;
+}
+
+
+// Печать суммы
+void printSumTo(std::int16_t sumValue) {
+  std::cout << sumValue;
+}
+
+
 int main() {
+  std::int16_t value    = readValue();
+  std::int16_t sumValue = sumTo(value);
+  printSumTo(sumValue);
 
   return 0;
 }
