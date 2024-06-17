81630a4cb830768f4be754e5464b41355eb1d246
Савченков Никита <savchienkov.nikita@mail.ru>
2024-06-17 22:39:01 +0300

    end write algorithm

--- simpleCalculatorV2.0/main.cpp
+++ simpleCalculatorV2.0/main.cpp
@@ -1,26 +1,43 @@
-#include <cstdint>
+#include <cassert>
 #include <iostream>
 
-
 //Считывание числа
-std::int64_t readNumber() {
-  std::int64_t number;
-  std::cin >> number;
-  return number;
+double getNumber() {
+  while (true) {
+    double number;
+    std::cin >> number;
+    // Проверка предыдущего извлечения
+    if (std::cin.fail()) {         // Если предыдущее извлечение оказалось неудачным
+      std::cin.clear();            // Возвращаем cin в обычный режим работы
+      std::cin.ignore(32767, '\n');// Удаляем значения предыдущего ввода и входного буфера
+      std::cout << "Input is invalid. Enter number again: ";
+    } else {
+      std::cin.ignore(32767, '\n');// Удаляем лишние значения
+      return number;
+    }
+  }
 }
 
 
 //Считывание оператора
-char readOperator() {
-  std::cout << "Enter math operator of the following: +, -, *, or /: ";
-  char mathOperator;
-  std::cin >> mathOperator;
-  return mathOperator;
+char getMathOperator() {
+  while (true) {
+    char mathOperator;
+    std::cin >> mathOperator;
+
+    std::cin.ignore(32767, '\n');
+
+    if (mathOperator == '+' || mathOperator == '-' || mathOperator == '*' || mathOperator == '/') {
+      return mathOperator;
+    } else {
+      std::cout << "Input is invalid. Enter operator again: ";
+    }
+  }
 }
 
 
 // Подсчёт результата выражения в зависимости от выбранного математического оператора
-void calculate(std::int64_t firstNumber, std::int64_t secondNumber, char mathOperator) {
+void calculate(double firstNumber, double secondNumber, char mathOperator) {
   switch (mathOperator) {
     case '+':
       std::cout << firstNumber + secondNumber;
@@ -34,10 +51,8 @@ void calculate(std::int64_t firstNumber, std::int64_t secondNumber, char mathOpe
     case '/':
       std::cout << firstNumber / secondNumber;
       break;
-    case '%':
-      std::cout << firstNumber % secondNumber;
-      break;
     default:
+      assert(false);
       std::cout << "Unknown operator\n";
       break;
   }
@@ -45,14 +60,17 @@ void calculate(std::int64_t firstNumber, std::int64_t secondNumber, char mathOpe
 
 
 int main() {
-  std::cout << "Enter first int number: ";
-  const std::int64_t firstNumber = readNumber();
-  std::cout << "Enter second int number: ";
-  const std::int64_t secondNumber = readNumber();
-  const char         mathOperator = readOperator();
+  std::cout << "Enter first number: ";
+  const double firstNumber = getNumber();
+  std::cout << "Enter math operator of the following: +, -, *, or /: ";
+  const char mathOperator = getMathOperator();
+  std::cout << "Enter second number: ";
+  const double secondNumber = getNumber();
+
 
   std::cout << firstNumber << ' ' << mathOperator << ' ' << secondNumber << " = ";
   calculate(firstNumber, secondNumber, mathOperator);
+  std::cout << '\n';
 
   return 0;
 }
